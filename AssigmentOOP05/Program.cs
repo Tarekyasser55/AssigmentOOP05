5f9cad6176ed03d4fde85e850c2180b71b267b31
Tarekyasser55 <Tarekyasser55@users.noreply.github.com>
2025-07-26 01:56:25 +0300

    finished solve assigment oop05

--- AssigmentOOP05/Program.cs
+++ AssigmentOOP05/Program.cs
@@ -5,33 +5,33 @@
         static void Main(string[] args)
         {
             #region FirstProject
-            Point3D p = new Point3D(10, 10, 10);
-            Console.WriteLine(p.ToString());
-            Point3D P1 = ReadPoint("P1");
-            Point3D P2 = ReadPoint("P2");
+            //Point3D p = new Point3D(10, 10, 10);
+            //Console.WriteLine(p.ToString());
+            //Point3D P1 = ReadPoint("P1");
+            //Point3D P2 = ReadPoint("P2");
 
-            Console.WriteLine(P1);
-            Console.WriteLine(P2);
+            //Console.WriteLine(P1);
+            //Console.WriteLine(P2);
 
-            Console.WriteLine("P1 == P2 " + (P1 == P2));
+            //Console.WriteLine("P1 == P2 " + (P1 == P2));
 
 
-            Point3D[] points = new Point3D[]
-            {
-                new Point3D(1,8,87),
-                new Point3D(32,55,99),
-                new Point3D(34,62,12),
-                new Point3D(80,57,71)
-            };
+            //Point3D[] points = new Point3D[]
+            //{
+            //    new Point3D(1,8,87),
+            //    new Point3D(32,55,99),
+            //    new Point3D(34,62,12),
+            //    new Point3D(80,57,71)
+            //};
 
-            Array.Sort(points);
+            //Array.Sort(points);
 
-            Console.WriteLine("array sorted\n");
-            foreach (var p in points)
-                Console.WriteLine(p);
+            //Console.WriteLine("array sorted\n");
+            //foreach (var p in points)
+            //    Console.WriteLine(p);
 
-            Point3D cloneP1 = (Point3D)P1.Clone();
-            Console.WriteLine("Cloned P1: " + cloneP1);
+            //Point3D cloneP1 = (Point3D)P1.Clone();
+            //Console.WriteLine("Cloned P1: " + cloneP1);
         
 
 
